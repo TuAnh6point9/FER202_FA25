747396568862ad62c24d1b8bec9685637e261012
TuAnh6point9 <nguyendinhanhtuan0609@gmail.com>
2025-10-01 11:17:24 +0700

    Intro . to Bootstrap

---
+++ slot7/ex7/src/App.js
@@ -0,0 +1,20 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import logo from './logo.svg';
+import './App.css';
+import Banner from './components/Banner';
+import Navar from './components/Navar';
+
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Navar />
+    </div>
+  );
+}
+
+export default App;
+
