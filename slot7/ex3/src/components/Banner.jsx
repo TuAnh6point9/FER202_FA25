7d2519ba00c292f8a32fef3e9b08492b083f2ee2
TuAnh6point9 <nguyendinhanhtuan0609@gmail.com>
2025-10-01 11:19:37 +0700

    Update ex3

---
+++ slot7/ex3/src/components/Banner.jsx
@@ -0,0 +1,9 @@
+export default function Banner() {
+    return (
+        <div className="container my-4">    
+            <div className="p-5 mb-4 bg-light rounded-3">
+                <h1 className="display-5 fw-bold mb-0">Let&apos;s test the grid</h1>
+                </div>          
+                </div>
+                );
+                }
